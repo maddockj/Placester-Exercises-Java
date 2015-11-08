7b0d1d1eb2e5871b0dc4cb1b8f4fea0e2ddcbdf3
maddockj <jordan.a.maddock@gmail.com>
2015-11-08 12:50:56 -0500

    Solved Weighted Die

--- src/main/com/placester/test/SixSidedWeightedDie.java
+++ src/main/com/placester/test/SixSidedWeightedDie.java
@@ -1,5 +1,7 @@
 package com.placester.test;
 
+import java.util.Random;
+
 /*
  * Implement a 6 sided die with weights on the sides, so that we don't have an even probability distribution, but it is 
  * weighted by a list of weights passed in at construction time
@@ -11,20 +13,59 @@ package com.placester.test;
  */
 public class SixSidedWeightedDie extends WeightedDie
 {
+	private final Random randomFloatGen = new Random();
+	private final float[] weights;
+	
     //NOTE: since these are weights on a probability distribution, these should sum to one, and the incoming array
     // should be of length 6. You should throw if either of these preconditions is false
     public SixSidedWeightedDie(float[] weights)
     {
         super(weights);
-        // TODO Auto-generated constructor stub
+        
+        // Validate input count
+        if (weights == null || weights.length != 6)
+        {
+        	throw new IllegalArgumentException("Six weights are required, not " + weights.length);
+        }
+        
+        // Validate input total
+        float weight = 0.00f;
+        for (float f : weights)
+        {
+        	weight += f;
+        }        
+        if (weight != 1.0f)
+        {
+        	throw new IllegalArgumentException("Total weight must be 1, not " + weight);
+        }
+        
+        // Good to go
+        this.weights = weights;
     }
 
     //Throw the die: this should produce a value in [1,6]
     @Override
     public int throwDie()
     {
-        // TODO Auto-generated method stub
-        return 0;
+    	// Create a random threshold between 0 and 1
+    	float threshold = randomFloatGen.nextFloat();
+    	
+    	// Iterate through the weights on each side, totaling until we pass the random threshold.
+    	// Works because larger weights have a greater chance of getting the total over the threshold.
+        float tempWeight = 0.0f;
+        for (int i = 0; i < 6; i++)
+        {
+        	tempWeight += weights[i];
+        	if (tempWeight >= threshold)
+        	{
+        		// The array is 0-based, but the die is 1-based, so add one for the return value
+        		return i+1;
+        	}
+        }
+        
+    	// Because the weight total equals 1 and the threshold is <= 1, we should never go past side 6
+    	// without meeting or crossing the threshold. Getting here means that assumption has failed.
+        throw new RuntimeException("Somthing went very unexpectedly wrong");
     }
 
 }
